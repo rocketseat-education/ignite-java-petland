5b725f62a24fc4744f54d11e6b66b0f699fabd48
vhraposo <thevhup@gmail.com>
2024-04-18 23:28:35 -0300

    Revisão de conteúdo

--- petland-api/src/main/java/com/petland/start/StartApp.java
+++ petland-api/src/main/java/com/petland/start/StartApp.java
@@ -1,30 +1,45 @@
 package com.petland.start;
 
-import java.time.LocalDate;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-import com.petland.model.Animal;
-import com.petland.model.AnimalEspecie;
 import com.petland.repository.AnimalRepository;
+import com.petland.repository.CadastroRepository;
 
 @Component
 public class StartApp implements ApplicationRunner {
 
     @Autowired
-    private AnimalRepository repository;
+    private CadastroRepository cadastroRepository;
+
+    @Autowired
+    private AnimalRepository animalRepository;
     
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        
-        Animal snoop = new Animal();
-        snoop.setNome("Snoop");
-        snoop.setAniversário(LocalDate.of(2015, 10, 10));
-        snoop.setEspecie(AnimalEspecie.CACHORRO);
+        // Cadastro victor = new Cadastro();
+        // victor.setNome("Victor");
+       
+        // Perfil perfil = new Perfil();
+        // perfil.setCliente(true);
+        // victor.setPerfil(perfil);
 
-        repository.save(snoop);
+        // Endereco endereco = new Endereco();
+        // endereco.setLogradouro("Rua das Flores");
+        // endereco.setNumero("123");
+        // victor.setEndereco(endereco);
+
+
+        // cadastroRepository.save(victor);
+
+
+        // Animal snoop = new Animal();
+        // snoop.setNome("Snoop");
+        // snoop.setAniversário(LocalDate.of(2015, 10, 10));
+        // snoop.setEspecie(AnimalEspecie.CACHORRO);
+
+        // animalRepository.save(snoop);
     }
 }
